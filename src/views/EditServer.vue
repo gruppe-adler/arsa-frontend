384e775c8737cf8dea754df7415c27719197a594
y0014984 <y0014984@gmail.com>
2024-10-21 20:03:57 +0200

    added server config upload/download

--- src/views/EditServer.vue
+++ src/views/EditServer.vue
@@ -18,26 +18,71 @@ if (result) server.value = result;
 
 const inputViolationCounter = ref(0);
 
-const disabled = computed<boolean>(() => (inputViolationCounter.value > 0) ? true : false );
+const disabled = computed<boolean>(() => (inputViolationCounter.value > 0) ? true : false);
 
 function updateServer() {
-  serversStore.update(server.value)
-      .then(() => {
-        router.push('/servers-list')
-      });
+    serversStore.update(server.value)
+        .then(() => {
+            router.push('/servers-list')
+        });
 }
+
+function downloadConfig(filename: string, data: any) {
+    const configFile = JSON.stringify(data, null, 2);
+
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(configFile));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+    document.body.removeChild(element);
+}
+
+const file = ref<File | null>();
+const fileSelector = ref<HTMLInputElement | null>(null);
+const uploadConfigDisabled = ref(true);
+
+function onFileChanged($event: Event) {
+    const target = $event.target as HTMLInputElement;
+    if (target && target.files) {
+        file.value = target.files[0];
+
+    }
+    uploadConfigDisabled.value = (target.value === '') ? true : false
+}
+
+function uploadConfig() {
+    if (file.value) {
+        file.value.text().then(content => server.value.config = JSON.parse(content));
+    }
+    if (fileSelector.value) fileSelector.value.value = '';
+}
+
+
 
 </script>
 
 <template>
-  <div class="form-container">
-    <h1>Edit Server</h1>
-    <ConfigForm v-model:input-violation-counter="inputViolationCounter" v-model:server="server"/>
-    <br/>
-    <br/>
-    <button type="button" :disabled @click="updateServer()">Update</button>
-  </div>
+    <div class="form-container">
+        <h1>Edit Server</h1>
+        <button class="form-input-button" type="button" @click="downloadConfig('server.json', server)">Download
+            Server</button>
+        <br />
+        <br />
+        <button class="form-input-button" type="button" @click="downloadConfig('config.json', server.config)">Download
+            Config</button>
+        <button class="form-input-button" type="button" @click="uploadConfig()" :disabled="uploadConfigDisabled">Upload
+            Config</button>
+        <input ref="fileSelector" class="form-input-button" type="file" @change="onFileChanged($event)"
+            accept="application/json" />
+        <ConfigForm v-model:input-violation-counter="inputViolationCounter" v-model:server="server" />
+        <br />
+        <br />
+        <button class="form-input-button" type="button" :disabled @click="updateServer()">Update</button>
+    </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
