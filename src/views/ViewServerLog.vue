9318fa37eaa8002f99c845f3ac42209cf62edb93
y0014984 <y0014984@gmail.com>
2024-10-23 20:53:59 +0200

    added refresh button for logs

--- src/views/ViewServerLog.vue
+++ src/views/ViewServerLog.vue
@@ -11,9 +11,14 @@ const route = useRoute();
 const serversStore = useServersStore();
 
 const serverLog = ref('')
-serversStore.getLog(route.params.id as string, 
+
+refresh();
+
+function refresh() {
+  serversStore.getLog(route.params.id as string, 
         route.params.log as string, 
         route.params.file as string).then((log) => serverLog.value = log.replace(/\n/g, '<br>'))
+}
 
 async function importPlayers() {
   const importedPlayers = await serversStore.getPlayersFromLog(route.params.id as string, route.params.log as string);
@@ -26,6 +31,7 @@ async function importPlayers() {
   <h1>View Server Log {{ route.params.log }}/{{ route.params.file }}</h1>
   <button type="button" @click="importPlayers()">Import Players</button>
   <p v-html="serverLog"></p>
+  <button type="button" @click="refresh()">Refresh</button>
 </template>
 
 <style scoped>
